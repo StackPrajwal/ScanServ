1af7795627b89b0a7f6c3c1ebaf2831a44f589dc
Prajwal Thombare <i.amprajwaljt@gmail.com>
2024-02-20 02:11:55 +0530

    Added Structure
    Added Basic Structure Of project

---
+++ ScanServMessMate/src/main/java/com/qspider/ScanServMessMate/ScanServMessMateApplication.java
@@ -0,0 +1,13 @@
+package com.qspider.ScanServMessMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanServMessMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanServMessMateApplication.class, args);
+	}
+
+}
---
+++ ScanServMessMate/src/test/java/com/qspider/ScanServMessMate/ScanServMessMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qspider.ScanServMessMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanServMessMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
